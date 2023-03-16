8703b98a5106c4f2c76bc04fd21441b5a66449fa
Chaitanya20044 <chaitanya20044@iiitd.ac.in>
2023-03-16 16:42:10 +0530

    Changes: 	modified:   Steamplague/Assets/Scenes/backupScene.unity 	modified:   Steamplague/Assets/Scripts/PlayerMovement.cs 	modified:   Steamplague/Assets/Scripts/ZombieAttack.cs 	modified:   Steamplague/ProjectSettings/TagManager.asset

--- Steamplague/Assets/Scripts/PlayerMovement.cs
+++ Steamplague/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float horizontalVelocity = 5f;
 
     private bool jumpPressed = false;
+    // private bool leftStopped = false;
+    // private bool rightStopped = false;
 
     private enum MovementState { idle, running, jumping, falling };
 
@@ -28,6 +30,16 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         float dirX = Input.GetAxis("Horizontal");
+        // if (leftStopped && dirX > 0f)
+        //     leftStopped = false;
+        // if (rightStopped && dirX < 0f)
+        //     rightStopped = false;
+            
+        // if (leftStopped && dirX < 0f)
+        //     dirX = 0;
+        // else if (rightStopped && dirX > 0f)
+        //     dirX = 0;
+
         if (player.velocity.x > horizontalVelocity)
             player.velocity = new Vector2(player.velocity.x - 0.01f, player.velocity.y);
         else if (player.velocity.x < -1 * horizontalVelocity)
@@ -37,6 +49,9 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
+            // leftStopped = false;
+            // rightStopped = false;
+
             float xVel = player.velocity.x;
             float yVel = jumpVelocity;
             if (xVel >= 4.5f || xVel <= -4.5f)
@@ -91,5 +106,70 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+    }
+
+    // void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (other.tag == "Terrain" || other.tag == "Zombie")
+    //     {
+    //         player.velocity = new Vector2(0, player.velocity.y);
+    //         // print(player.transform.position.x + ", " + other.transform.position.x);
+    //         // if (player.transform.position.x > other.transform.position.x)
+    //         //     rightStopped = true;
+    //         // else if (player.transform.position.x < other.transform.position.x)
+    //         //     leftStopped = true;
+    //         if (player.velocity.x > 0)
+    //             rightStopped = true;
+    //         else if (player.velocity.x < 0)
+    //             leftStopped = true;
+    //     }
+    // }
+
+    // void OnTriggerExit2D(Collider2D other)
+    // {
+    //     if (other.tag == "Terrain" || other.tag == "Zombie")
+    //     {
+    //         rightStopped = false;
+    //         leftStopped = false;
+    //     }
+    // }
+    
+    void OnCollisionEnter2D(Collision2D coll) 
+    {
+        // Collider2D collider = coll.collider;
+
+        // if(collider.tag == "Terrain")
+        // { 
+        //     Vector3 contactPoint = coll.contacts[0].point;
+        //     Vector3 center = collider.bounds.center;
+
+        //     bool right = contactPoint.x > center.x;
+        //     bool top = contactPoint.y > center.y;
+
+        //     if (contactPoint.x > center.x || contactPoint.x < center.x)
+        //     {
+        //         print("collided");
+        //         player.velocity = new Vector2(0, player.velocity.y);
+        //     }
+        // }
+
+        // if (coll.gameObject.tag == "Terrain")
+        // {
+        //     if ((this.transform.position.x - coll.transform.position.x) < 0)
+        //     {
+        //         print("hit left" + this.transform.position.x + ", " + coll.collider.transform.position.x);
+        //         player.velocity = new Vector2(0, player.velocity.y);
+        //     }
+        //     else if ((this.transform.position.x - coll.transform.position.x) > 0)
+        //     {
+        //         print("hit right" + this.transform.position.x + ", " + coll.collider.transform.position.x);
+        //         player.velocity = new Vector2(0, player.velocity.y);
+        //     }
+        // }
+
+        // if (coll.gameObject.tag == "Zombie")
+        // {
+        //     // print("zombie hit");
+        // }
     }
 }
--- Steamplague/Assets/Scripts/ZombieAttack.cs
+++ Steamplague/Assets/Scripts/ZombieAttack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ZombieAttack : MonoBehaviour
 {
-    private Rigidbody2D zombie;
+    // private Rigidbody2D zombie;
     // private Animator zombieAnim;
     [SerializeField] private SpriteRenderer zombieSprite;
     [SerializeField] private BoxCollider2D zombieCollider;
@@ -15,12 +15,13 @@ public class ZombieAttack : MonoBehaviour
     [SerializeField] private float range = 3f;
     [SerializeField] private float colliderDistance = 1f;
 
-    private float cooldownTimer = Mathf.Infinity; 
+    private float cooldownTimer = Mathf.Infinity;
+    private bool canAttack = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        zombie = GetComponent<Rigidbody2D>();
+        // zombie = GetComponent<Rigidbody2D>();
         // zombieAnim = GetComponent<Animator>();
         // zombieSprite = GetComponent<SpriteRenderer>();
         // zombieCollider = GetComponent<BoxCollider2D>();
@@ -29,34 +30,68 @@ public class ZombieAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        cooldownTimer += Time.deltaTime;
-        if (PlayerInRange())
+        if (canAttack)
         {
+            cooldownTimer += Time.deltaTime;
             if (cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
                 // TODO: Attack
-                print("Attack");
+                Attack();
             }
         }
 
     }
 
-    private bool PlayerInRange()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        int dirX = 1;
-        if (zombieSprite.flipX)
-            dirX = -1;
-        RaycastHit2D hit = Physics2D.BoxCast(zombieCollider.bounds.center + (dirX * transform.right * range) * colliderDistance, new Vector3(zombieCollider.bounds.size.x * range, zombieCollider.bounds.size.y, zombieCollider.bounds.size.z), 0f, Vector2.left, 0f, playerLayer);
-        return (hit.collider != null);
+        if (other.tag == "Player")
+        {        
+            print("enter");
+            cooldownTimer = 0;
+            canAttack = true;
+        }
     }
 
-    private void OnDrawGizmos()
+    void OnTriggerStay2D(Collider2D other)
     {
-        Gizmos.color = Color.red;
-        int dirX = 1;
-        if (zombieSprite.flipX)
-            dirX = -1;
-        Gizmos.DrawWireCube(zombieCollider.bounds.center + (dirX * transform.right * range) * colliderDistance, new Vector3(zombieCollider.bounds.size.x * range, zombieCollider.bounds.size.y, zombieCollider.bounds.size.z));
+        if (other.tag == "Player")
+        {
+            // print("stay");
+        }
     }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            print("exit");
+            cooldownTimer = 0;
+            canAttack = false;
+        }
+    }
+
+    private bool PlayerInRange()
+    {
+        // int dirX = 1;
+        // if (zombieSprite.flipX)
+        //     dirX = -1;
+        // RaycastHit2D hit = Physics2D.BoxCast(zombieCollider.bounds.center + (dirX * transform.right * range) * colliderDistance, new Vector3(zombieCollider.bounds.size.x * range, zombieCollider.bounds.size.y, zombieCollider.bounds.size.z), 0f, Vector2.left, 0f, playerLayer);
+        // return (hit.collider != null);
+        return false;
+    }
+
+    private void Attack()
+    {
+        print("Attacking player");
+    }
+
+    // private void OnDrawGizmos()
+    // {
+    //     Gizmos.color = Color.red;
+    //     int dirX = 1;
+    //     if (zombieSprite.flipX)
+    //         dirX = -1;
+    //     Gizmos.DrawWireCube(zombieCollider.bounds.center + (dirX * transform.right * range) * colliderDistance, new Vector3(zombieCollider.bounds.size.x * range, zombieCollider.bounds.size.y, zombieCollider.bounds.size.z));
+    // }
 }
